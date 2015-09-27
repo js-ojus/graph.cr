34b2c93663f36ff7a91932ec9d0df86b99a95dd4
JONNALAGADDA Srinivas <js@ojuslabs.com>
2015-09-27 16:02:58 +0530

    Make specs work after renaming

--- src/graph/data/entity.cr
+++ src/graph/data/entity.cr
@@ -13,6 +13,7 @@
 # under the License.
 
 module Graph::Data
+  extend self
 
   # Entity defines the basic data elements that are common to all
   # entities (but not associations.)
@@ -32,31 +33,5 @@ module Graph::Data
       getter id, name, provider, provider_id
     end
   end
-
-  # AdverseEffect represents an unwanted side effect caused by a
-  # particular drug.  Adverse effects form part of a controlled
-  # vocabulary.
-  def_entity :AdverseEffect
-
-  # Disease represents a disease condition.  It acts as one of the
-  # major hubs in the graph.  It is an intermediate layer in
-  # transitive relationships between targets, drugs, pathways, adverse
-  # effects, etc.
-  def_entity :Disease
-
-  # Drug represents either a clinical drug, or a pre-clinical,
-  # depending on the context.  It acts as one of the major hubs in the
-  # graph.  It is an intermediate layer in transitive relationships
-  # between targets, diseases, pathways, adverse effects, etc.
-  def_entity :Drug
-
-  # Pathway represents a biochemical pathway in an organism.  It may
-  # involve multiple targets that can be acted upon by drugs.
-  def_entity :Pathway
-
-  # Target represents a biological gene that can either participate in
-  # a pathway, or affect one, or be a target of one or more drug
-  # molecules.
-  def_entity :Target
 
 end
