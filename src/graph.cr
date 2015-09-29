adc731c55be13edc42c71f7c79a799ebb68a676e
JONNALAGADDA Srinivas <js@ojuslabs.com>
2015-09-29 12:30:33 +0530

    Make `graph.cr` recursively require other files
    * Remove `graph/data.cr` accordingly.

--- src/graph.cr
+++ src/graph.cr
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-require "./graph/*"
+require "./graph/**"
 
 module Graph
   # TODO Put your code here
