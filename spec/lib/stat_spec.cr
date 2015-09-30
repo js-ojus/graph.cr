58517a2bf1efd1293cedd911ccc4c48b38222855
JONNALAGADDA Srinivas <js@ojuslabs.com>
2015-09-30 11:06:16 +0530

    Add more tests for chi-square and p-value computations

--- spec/lib/stat_spec.cr
+++ spec/lib/stat_spec.cr
@@ -16,4 +16,18 @@ describe "p-value calculation" do
   it "checks reliability" do
     t[2].should eq(true)
   end
+
+  t = Stat.chi_square_2x2(c_11: 20_u64, c_12: 30_u64, c_21: 30_u64, c_22: 20_u64)
+
+  it "checks chi-square value" do
+    ((t[0] - 4.0).abs < 0.01).should eq(true)
+  end
+
+  it "checks p-value" do
+    t[1].should eq(0.05)
+  end
+
+  it "checks reliability" do
+    t[2].should eq(true)
+  end
 end
